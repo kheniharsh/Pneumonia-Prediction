48083d26c6ec99bc7750fdda31489c5bfb4a0ae7
kheniharsh <53462199+kheniharsh@users.noreply.github.com>
2022-03-19 11:45:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 import numpy as np
-import cv2
-from flask import *
-from tensorflow import keras
+from PIL import Image, ImageOps
+from flask import Flask,render_template,request
+
+from keras import models
+model_ct = models.load_model('prediction-ct.h5')
 
 app = Flask(__name__)
-
-model_ct = keras.models.load_model('prediction_ct.h5')
 
 @app.route('/')
 def home():
@@ -14,22 +14,27 @@ def home():
 @app.route('/predict',methods=['POST'])
 def predict():
     if request.method == 'POST':
-        predict_ct = []
         file = request.files['imagefile']
-        img = cv2.imread(file)
-        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        img = cv2.resize(gray,(150,150))
-        predict_ct.append(img)
-        predict_ct = np.array(predict_ct)
-        predict_ct = predict_ct.reshape(predict_ct.shape[0], 150, 150,1)
+    
+    predict_ct = []    
+    img = Image.open(file)
+    gray = ImageOps.grayscale(img)
+    img = gray.resize((150,150), Image.ANTIALIAS)
+    imgs = np.asarray(img)
+    predict_ct.append(imgs)
+    predict_ct = np.array(predict_ct)
+    predict_ct = predict_ct.reshape(predict_ct.shape[0], 150, 150,1)
 
-        predict_x=model_ct.predict(predict_ct) 
-        classes_x=np.argmax(predict_x, axis=1)
-        classes_x
-        if classes_x[0]==0:
-            x='Your Report is Normal'
-            print(x)
-        else:
-            x='Oops, Repot Consist Pneumonia, Consult a Doctor'
-            print(x)
+    predict_x=model_ct.predict(predict_ct) 
+    classes_x=np.argmax(predict_x, axis=1)
+    classes_x
+    if classes_x[0]==0:
+        x='Your Report is Normal'
+    else:
+        x='Oops. Report Consist Pneumonia, Consult a Doctor'
+
     return render_template('home.html', val=x)
+
+
+if __name__ == '__main__':
+    app.run()
